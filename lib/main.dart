6ab4e89446d9e38382757857aa9c7b36b4c2dc2a
Merahi Yacine <yacinekadda8@gmail.com>
2023-10-03 18:11:48 +0100

    website

--- lib/main.dart
+++ lib/main.dart
@@ -23,7 +23,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      title: 'Flutter Demo',
+      title: 'Portfolio',
       theme: ThemeData.dark().copyWith(
         primaryColor: kprimaryColor,
         scaffoldBackgroundColor: bgColor,
--- lib/screens/components/Side Menu/socialmedia.dart
+++ lib/screens/components/Side Menu/socialmedia.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_svg/svg.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:url_launcher/url_launcher.dart';
 
